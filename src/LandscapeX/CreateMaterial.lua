285904bc64822ebc69cd4f06902b7412259f7335
ArsonistD <39225423+arsonistd@users.noreply.github.com>
2021-12-15 07:59:32 -0500

    Update CreateMaterial.lua

--- src/LandscapeX/CreateMaterial.lua
+++ src/LandscapeX/CreateMaterial.lua
@@ -6,8 +6,19 @@
 local CreateMaterial = {}
 CreateMaterial.__index = CreateMaterial
 
-function CreateMaterial.new()
+function CreateMaterial.new(data)
     local self = setmetatable({}, CreateMaterial)
+    
+    if data == nil then
+       data = {} 
+    end
+    
+    self.Material = data["Material"] or "Test" --edit
+    self.MaxHeight = data["MaxHeight"] or 50
+    self.MinHeight = data["MinHeight"] or 0
+    self.MaxSlope = data["MaxSlope"] or 90
+    self.MinSlope = data["MinSlope"] or 0
+    
     return self
 end
 
