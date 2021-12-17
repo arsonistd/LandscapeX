a14fa953780316d3acdc510a6a8329ecab885661
ArsonistD <39225423+arsonistd@users.noreply.github.com>
2021-12-17 08:07:57 -0500

    Update Values.lua

--- src/LandscapeX/Values.lua
+++ src/LandscapeX/Values.lua
@@ -1,7 +1,29 @@
 local Values = {}
 
 Values.Materials = {
-	
+	["Air"] = {1792, "rbxassetid://7227350355"},
+	["Asphalt"] = {1376, "rbxasset://textures/TerrainTools/mtrl_asphalt.png"},
+	["Basalt"] = {788, "rbxasset://textures/TerrainTools/mtrl_basalt.png"},
+	["Brick"] = {848, "rbxasset://textures/TerrainTools/mtrl_brick.png"},
+	["Cobblestone"] = {880, "rbxasset://textures/TerrainTools/mtrl_cobblestone.png"},
+	["Concrete"] = {816, "rbxasset://textures/TerrainTools/mtrl_concrete.png"},
+	["Cracked Lava"] = {804, "rbxasset://textures/TerrainTools/mtrl_crackedlava.png"},
+	["Glacier"] = {1552, "rbxasset://textures/TerrainTools/mtrl_glacier.png"},
+	["Grass"] = {1280, "rbxasset://textures/TerrainTools/mtrl_grass.png"},
+	["Ground"] = {1360, "rbxasset://textures/TerrainTools/mtrl_ground.png"},
+	["Ice"] = {1536, "rbxasset://textures/TerrainTools/mtrl_ice.png"},
+	{820, "rbxasset://textures/TerrainTools/mtrl_limestone.png"},
+	{1344, "rbxasset://textures/TerrainTools/mtrl_mud.png", "Mud"},
+	{836, "rbxasset://textures/TerrainTools/mtrl_pavement.png", "Pavement"},
+	{896, "rbxasset://textures/TerrainTools/mtrl_rock.png", "Rock"},
+	{1392, "rbxasset://textures/TerrainTools/mtrl_salt.png", "Salt"},
+	{1296, "rbxasset://textures/TerrainTools/mtrl_sand.png", "Sand"},
+	{912, "rbxasset://textures/TerrainTools/mtrl_sandstone.png", "Sandstone"},
+	{800, "rbxasset://textures/TerrainTools/mtrl_slate.png", "Slate"},
+	{1328, "rbxasset://textures/TerrainTools/mtrl_snow.png", "Snow"},
+	{2048, "rbxasset://textures/TerrainTools/mtrl_water.png", "Water"},
+	{528, "rbxasset://textures/TerrainTools/mtrl_woodplanks.png", "Wooden Planks"},
+}
 }
 
 Values.Noises = {
